248f1fda6c84d4172a4cb2744281231e903cb1eb
Agustin <agualvila@gmail.com>
2022-02-05 19:52:13 -0300

    tasks bussines logic & verifications

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,41 +6,49 @@ import {
   Delete,
   Body,
   Param,
+  UseGuards,
+  Req,
+  HttpStatus,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  async getAll() {
-    return this.tasksService.findAll();
+  async getAll(@Req() request): Promise<any> {
+    const { userId } = request.user;
+    return await this.tasksService.findAll(userId);
   }
 
   @Get(':id')
-  async getOne(@Param('id') id: number, @Body('userId') userId: number) {
-    // return { userId, taskId };
+  async getOne(@Param('id') id: number, @Req() request): Promise<any> {
+    const { userId } = request.user;
     return await this.tasksService.findOne(id, userId);
   }
 
   @Post()
   async create(
-    @Body('userId') userId: number,
+    @Req() request,
     @Body('name') name: string,
     @Body('description') description: string,
   ) {
+    const { userId } = request.user;
     return await this.tasksService.create(userId, { name, description });
   }
 
   @Patch(':id')
   async update(
+    @Req() request,
     @Param('id') id: number,
-    @Body('userId') userId: number,
     @Body('name') name: string,
     @Body('description') description: string,
     @Body('completed') completed: boolean,
   ) {
+    const { userId } = request.user;
     return await this.tasksService.update(id, userId, {
       name,
       description,
@@ -49,7 +57,12 @@ export class TasksController {
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: number, @Body('userId') userId: number) {
-    return await this.tasksService.delete(id, userId);
+  async delete(@Param('id') id: number, @Req() request) {
+    const { userId } = request.user;
+    const response = await this.tasksService.delete(id, userId);
+    return {
+      statusCode: HttpStatus.OK,
+      message: response,
+    };
   }
 }
