0ca689f96ebc200e4535f6a30a5b1be922f4b015
Agustin <agualvila@gmail.com>
2022-02-05 10:31:57 -0300

    auth bcrypt service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Observable, from } from 'rxjs';
+
+import bcrypt = require('bcrypt');
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  hashPassword(password: string): Observable<string> {
+    return from<string>(bcrypt.hash(password, 12));
+  }
+
+  comparePasswords(password: string, dbPassword: string): Observable<any> {
+    return from(bcrypt.compare(password, dbPassword));
+  }
+}
